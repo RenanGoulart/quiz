061576577715f7e71b394c18ea47f5f02bd43905
Renan Goulart <64268235+RenanGoulart@users.noreply.github.com>
2022-04-29 13:26:51 -0300

    feat: functionality to the quiz

--- src/App.js
+++ src/App.js
@@ -1,42 +1,89 @@
 import React from 'react';
+import Radio from './Components/Radio';
 
-const perguntas = [
+const questions = [
   {
-    pergunta: 'Qual método é utilizado para criar componentes?',
+    sentence: 'Qual método é utilizado para criar componentes?',
     options: [
       'React.makeComponent()',
       'React.createComponent()',
       'React.createElement()',
     ],
-    resposta: 'React.createElement()',
+    answer: 'React.createElement()',
     id: 'p1',
   },
   {
-    pergunta: 'Como importamos um componente externo?',
+    sentence: 'Como importamos um componente externo?',
     options: [
       'import Component from "./Component"',
       'require("./Component")',
       'import "./Component"',
     ],
-    resposta: 'import Component from "./Component"',
+    answer: 'import Component from "./Component"',
     id: 'p2',
   },
   {
-    pergunta: 'Qual hook não é nativo?',
+    sentence: 'Qual hook não é nativo?',
     options: ['useEffect()', 'useFetch()', 'useCallback()'],
-    resposta: 'useFetch()',
+    answer: 'useFetch()',
     id: 'p3',
   },
   {
-    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    sentence: 'Qual palavra deve ser utilizada para criarmos um hook?',
     options: ['set', 'get', 'use'],
-    resposta: 'use',
+    answer: 'use',
     id: 'p4',
   },
 ];
 
-function App() {
-  return <div>Inicio</div>;
-}
+const App = () => {
+  const [answers, setAnswers] = React.useState(
+    questions.reduce((acc, answer) => {
+      return { ...acc, [answer.id]: '' };
+    }, {}),
+  );
+  const [slide, setSlide] = React.useState(0);
+  const [result, setResult] = React.useState(null);
+
+  function handleChange({ target }) {
+    setAnswers({ ...answers, [target.id]: target.value });
+  }
+
+  function compareResult() {
+    const correct = questions.filter(
+      ({ id, answer }) => answers[id] === answer,
+    );
+    setResult(`Você acertou: ${correct.length} de ${questions.length}.`);
+  }
+
+  function handleNext() {
+    if (slide < questions.length - 1) {
+      setSlide(slide + 1);
+    } else {
+      setSlide(slide + 1);
+      compareResult();
+    }
+  }
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      {questions.map((question, index) => (
+        <Radio
+          active={slide === index}
+          onChange={handleChange}
+          value={answers[question.id]}
+          key={question.id}
+          {...question}
+        />
+      ))}
+      {result ? <p>{result}</p> : <button onClick={handleNext}>Próxima</button>}
+    </form>
+  );
+};
 
 export default App;
+// evitar olhar material dele, usar as docs feitas na pasta
